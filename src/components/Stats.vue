dbd3f52482536922c1fc1f52aa1db0d9df9d932b
HenriqueFutema <minifut0407@gmail.com>
2019-07-11 00:42:02 -0300

    Estudando Vuex

---
+++ src/components/Stats.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="stats">
+    <h1>Quantidades de Links: {{ countLinks }}</h1>
+    <button @click="removeAllLinks">Remover todos o Links</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex";
+export default {
+  name: "Stats",
+  computed: {
+    ...mapGetters(["countLinks"])
+  },
+  methods: {
+    ...mapMutations(["REMOVE_ALL"]),
+    removeAllLinks() {
+      this.REMOVE_ALL();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+button {
+  padding: 10px;
+  margin-top: 30px;
+  width: 100%;
+  background: none;
+  border: 1px solid lightgray;
+  outline: 0;
+  cursor: pointer;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -4,13 +4,33 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        title: 'Testando o VueX',
+        links: [
+            'http://google.com',
+            'http://coursetro.com',
+            'http://youtube.com'
+        ]
+    },
+    getters: {
+        countLinks: state => {
+            return state.links.length
+        }
+    },
+    mutations: {
+        ADD_LINK: (state, link) => {
+            state.links = [link, ...state.links]
+        },
+        REMOVE_LINK: (state, link) => {
+            state.links.splice(link, 1)
+        },
+        REMOVE_ALL: state => {
+            state.links = []
+        }
+    },
+    actions: {
+        removeLink: (context, link) => {
+            context.commit('REMOVE_LINK', link)
+        }
+    }
 })
